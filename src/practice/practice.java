2ab7fb3be74dbcb6ea0ee2e67258b8eaa183d3d4
hnyrk <hnyrk1telolalb@gmail.com>
2023-06-06 00:18:28 +0900

    キーボード受付

--- src/practice/practice.java
+++ src/practice/practice.java
@@ -1,15 +1,20 @@
 package practice;
 
+import java.util.Scanner;
+
 public class practice {
 
 	public static void main(String[] args) {
-		int[] array = new int[10];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = i;
-		}
-		for (int i = array.length - 1; i >= 0; i--) {
-			System.out.println(array[i]);
-		}
+		 Scanner sc = new Scanner(System.in);
+	        // 5個のデータを入力する
+	        int x[]  = new int[5];
+	        for (int i = 0; i < 5; i++) {
+	            x[i]  = sc.nextInt();
+	        }
+	        // 逆順に出力
+	        for (int i = 4; i >= 0; i--) {
+	            System.out.print(x[i] + " ");
+	        }
 	}
 
 }
