4aaabfb32292f6b3a5010ed423f62b1a2887d466
hnyrk <hnyrk1telolalb@gmail.com>
2023-06-07 00:53:55 +0900

    1〜10までの整数をカンマ区切りで横並びで表示させる

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,22 +1,13 @@
 package practice;
 
-//1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる
+//1〜10までの整数をカンマ区切りで横並びで表示させる
 
 public class Practice {
 	public static void main(String[] args) {
 		int[] array = new int[10];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = i + 1;
+			System.out.print(array[i] + ",");
 		}
-		int count = 0;
-		int sum = 0;
-		for (int value : array) {
-			if (value % 2 == 0) {
-				count++;
-				sum += value;
-			}
-		}
-		System.out.println("個数：" + count);
-		System.out.println("合計：" + sum);
 	}
 }
