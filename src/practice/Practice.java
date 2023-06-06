80aa2f9c70a7805e879482431511ccc743d02ee2
hnyrk <hnyrk1telolalb@gmail.com>
2023-06-07 00:51:02 +0900

    1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,18 +1,22 @@
 package practice;
 
-//1〜10の数字の偶数のみを大きい順に上から縦に並ぶように表示させる
+//1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる
 
 public class Practice {
-
 	public static void main(String[] args) {
 		int[] array = new int[10];
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < array.length; i++) {
 			array[i] = i + 1;
 		}
-		for (int i = array.length - 1; i >= 0; i--) {
-			if (array[i] % 2 == 0) {
-				System.out.println(array[i]);
+		int count = 0;
+		int sum = 0;
+		for (int value : array) {
+			if (value % 2 == 0) {
+				count++;
+				sum += value;
 			}
 		}
+		System.out.println("個数：" + count);
+		System.out.println("合計：" + sum);
 	}
 }
