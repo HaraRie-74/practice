d50245a5e472a8db89eb0619f77b16a15d446ff9
hnyrk <hnyrk1telolalb@gmail.com>
2023-06-07 00:13:28 +0900

    配列　昇順・降順

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,26 +1,20 @@
 package practice;
+import java.util.Arrays;
 
 public class Practice {
 
 	public static void main(String[] args) {
-		 int[] array = new int[10];
-		 for (int i = 0; i < array.length; i++) {
-			 array[i] = i;
-		 }
-		 System.out.print("偶数：");
-		 for (int value : array) {
-			 if (value % 2 == 0) {
-				 System.out.print(value + " ");
-			 }
-		 }
-		 System.out.println(" ");
-		 
-		 System.out.print("奇数：");
-		 for (int value : array) {
-			 if (value % 2 == 1) {
-				 System.out.print(value + " ");
-			 }
-		 }
+		int[] array = new int[6];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random() * 11);
+		}
+		System.out.println(Arrays.toString(array));
+		
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+		
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.println(array[i]);
+		}
 	}
-
 }
